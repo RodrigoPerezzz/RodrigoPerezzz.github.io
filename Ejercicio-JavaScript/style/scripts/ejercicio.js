46c2b7b9fcf0064dfd6cf9522c18bdb332ed4abe
RodrigoPerezzz <162516686+RodrigoPerezzz@users.noreply.github.com>
2024-03-08 14:23:45 -0600

    Add files via upload

---
+++ Ejercicio-JavaScript/style/scripts/ejercicio.js
@@ -0,0 +1,19 @@
+
+function cambiarColor(){
+    variable_album=document.getElementById("album");
+    variable_album.style.backgroundColor="green";
+    console.log(variable_album);
+}
+
+function cambiarTamano(){
+    
+const tamano=document.getElementsByClassName("imagen");
+const tamano1= tamano[0];
+tamano1.style.width="50px";
+tamano1.style.height="auto";
+console.log(tamano1);
+   
+   
+    
+}
+
